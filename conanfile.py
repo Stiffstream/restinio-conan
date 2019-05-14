2f957de8f7791fb66c952a5c57242028ea7aca5b
eao197 <eao197@gmail.com>
2019-05-14 09:16:43 +0300

    RESTinio updated to v.0.4.9

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ import os
 
 class SobjectizerConan(ConanFile):
     name = "restinio"
-    version = "0.4.8.7"
+    version = "0.4.9"
 
     license = "BSD-3-Clause"
     url = "https://github.com/Stiffstream/restinio-conan"
