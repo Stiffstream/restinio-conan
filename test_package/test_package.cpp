99ffe8c93bf82e5ed957ddbd644e7044cdb8c128
Christopher McArthur <prince.chrismc@gmail.com>
2020-05-11 14:52:10 -0400

    adding a test package
    to ensure package created is usable

---
+++ test_package/test_package.cpp
@@ -0,0 +1,13 @@
+#include <restinio/all.hpp>
+
+int main()
+{
+	restinio::on_this_thread<>()
+		.port(8080)
+		.address("localhost")
+		.request_handler([](auto req) {
+			return req->create_response().set_body("Hello, World!").done();
+		});
+
+	return 0;
+}
