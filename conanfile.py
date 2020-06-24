52610ad945beb136541b71f36c3f2c3698c9f57b
Yauheni Akhotnikau <eao197@gmail.com>
2020-06-24 17:44:06 +0300

    RESTinio updated to v.0.6.8.1.

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ import os
 
 class RestinioConan(ConanFile):
     name = "restinio"
-    version = "0.6.8"
+    version = "0.6.8.1"
     license = "BSD-3-Clause"
     url = "https://github.com/Stiffstream/restinio-conan"
     homepage = "https://github.com/Stiffstream/restinio"
